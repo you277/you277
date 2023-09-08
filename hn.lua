81ab23df09b525e0c6542f6526aea88c52589007
you <144391848+you277@users.noreply.github.com>
2023-09-08 15:34:47 -0400

    hn2

--- hn.lua
+++ hn.lua
@@ -1,3 +1,18 @@
+--
+-- Supernull, old Hypernull (bindableHypernull), and Hypernull
+--
+
+local function recurse(depth, callback, ...)
+    if depth == 80 then
+        return callback(...)
+    end
+    task.defer(recurse, depth + 1, callback, ...)
+end
+
+local function supernull(callback, ...)
+    recurse(0, callback, ...)
+end
+
 local function hypernull(callback, ...)
     local v = true
     task.spawn(function()
@@ -9,4 +24,20 @@ local function hypernull(callback, ...)
     hypernull(callback, ...)
 end
 
-return hypernull
+local bindable = Instance.new("BindableFunction")
+local invoke = bindable.Invoke
+bindable.OnInvoke = function(callback, ...)
+    if not pcall(invoke, bindable, callback, ...) then
+        callback(...)
+    end
+end
+
+local function bindableHypernull(callback, ...)
+    invoke(bindable, callback, ...)
+end
+
+return {
+    HN = hypernull,
+    BHN = bindableHypernull,
+    SN = supernull,
+}
